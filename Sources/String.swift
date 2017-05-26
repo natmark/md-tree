53f834846f4c8164f161724ca4fa098bde7df418
natmark <natmark0918@gmail.com>
2017-05-26 13:30:12 +0900

    Add String extension

---
+++ Sources/String.swift
@@ -0,0 +1,26 @@
+//
+//  String.swift
+//  md-tree
+//
+//  Created by AtsuyaSato on 2017/05/25.
+//
+//
+
+import Foundation
+
+extension String{
+    func headLineLevel() -> Int{
+        do {
+            let regxp = try NSRegularExpression(
+                pattern: "(#*)\\s.*", options: [])
+            let result = regxp.firstMatch(in: self, options: [], range: NSMakeRange(0, (self as NSString).length))
+            guard let headLineRange = result?.rangeAt(1) else { return -1 }
+            
+            let headLine = String(NSString(string: self).substring(with: headLineRange))!
+            
+            return headLine.characters.count
+        } catch {
+            return -1
+        }
+    }
+}
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,20 +1,56 @@
 import Foundation
 import OptionKit
 
-// md-tree -f filename.md 大見出し 中見出し 小見出し
-let arguments = Array(CommandLine.arguments.dropFirst())
-
-let fileOption = Option(trigger: OptionTrigger.Mixed("f", "filename"), numberOfParameters: 1, helpDescription: "Markdown file")
-let parser = OptionParser(definitions: [fileOption])
-
-do {
-    let (options, _) = try parser.parse(parameters: arguments)
-    if let filePath = options[fileOption]?.first {
-        print(filePath)
-        let hierarchy = arguments.dropFirst(2)
-        print(hierarchy)
-    }
-} catch let OptionKitError.InvalidOption(description: description) {
-    print(description)
-    exit(EXIT_FAILURE)
+enum LineType {
+    case headLine
+    case content
 }
+class Node {
+    let childs = [Node]()
+    let parent:Node?
+    
+    var type: LineType? = nil
+    var line: String = ""
+    
+    init(parent: Node?){
+        self.parent = parent
+        parent?.childs.append(self)
+    }
+}
+// md-tree -f filename.md 大見出し 中見出し 小見出し
+private func main(arguments: [String]) {
+    let arguments = arguments.dropFirst()
+
+    let fileOption = Option(trigger: OptionTrigger.Mixed("f", "filename"), numberOfParameters: 1, helpDescription: "Markdown file")
+    let parser = OptionParser(definitions: [fileOption])
+
+    do {
+        let (options, _) = try parser.parse(parameters: Array(arguments))
+        if let filePath = options[fileOption]?.first {
+            print(filePath)
+            let location = NSString(string: filePath).expandingTildeInPath
+            let fileContent = try String(contentsOfFile: location, encoding: String.Encoding.utf8)
+            let hierarchy = Array(arguments.dropFirst(2))
+
+            _ = extract(text: fileContent,hierarchy: hierarchy)
+        }
+    } catch let OptionKitError.InvalidOption(description: description) {
+        print(description)
+        exit(EXIT_FAILURE)
+    } catch {
+        exit(0)
+    }
+}
+
+func extract(text: String, hierarchy: [String]) -> String{
+    var current = Node(parent: nil)
+    
+    text.enumerateLines(invoking: { line, stop in
+        let trimmedLine = line.trimmingCharacters(in: .whitespaces)
+        if trimmedLine.isEmpty { return }
+        let type:LineType = trimmedLine.headLineLevel() == 0 ? .content : .headLine
+    })
+    return ""
+}
+
+main(arguments: CommandLine.arguments)
