83bcff9c16fd66e7e29a492b11078f0a97136950
natmark <natmark0918@gmail.com>
2017-05-31 11:49:50 +0900

    :sparkles: Add option parameter

--- Sources/String.swift
+++ Sources/String.swift
@@ -8,19 +8,31 @@
 
 import Foundation
 
-extension String{
-    func headLineLevel() -> Int{
+extension String {
+    var headLineLevel: Int {
         do {
             let regxp = try NSRegularExpression(
                 pattern: "(#*)\\s.*", options: [])
             let result = regxp.firstMatch(in: self, options: [], range: NSMakeRange(0, (self as NSString).length))
             guard let headLineRange = result?.rangeAt(1) else { return 0 }
-            
+
             let headLine = String(NSString(string: self).substring(with: headLineRange))!
-            
             return headLine.characters.count
         } catch {
-            return 0
+            exit(EXIT_FAILURE)
+        }
+    }
+    var planeText: String {
+        do {
+            let regxp = try NSRegularExpression(
+                pattern: "\\s(.*)", options: [])
+            let result = regxp.firstMatch(in: self, options: [], range: NSMakeRange(0, (self as NSString).length))
+            guard let textRange = result?.rangeAt(1) else { return self }
+
+            let text = String(NSString(string: self).substring(with: textRange))!
+            return text
+        } catch {
+            exit(EXIT_FAILURE)
         }
     }
 }
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,12 +1,28 @@
 import Foundation
 import OptionKit
 
+enum ConsoleOption {
+    case file
+    case tab
+    case emptyline
+
+    var option: Option {
+        switch self {
+        case .file:
+            return Option(trigger: .Mixed("f", "filename"), numberOfParameters: 1, helpDescription: "Markdown file")
+        case .tab:
+            return Option(trigger:.Mixed("t", "tab"))
+        case .emptyline:
+            return Option(trigger:.Mixed("e", "emptyline"))
+        }
+    }
+}
 enum LineType {
     case headLine(Int)
     case content
     case none
-    
-    func level() -> Int {
+
+    var level: Int {
         switch self {
         case .headLine(let headLevel):
             return headLevel
@@ -16,67 +32,107 @@ enum LineType {
             return 0
         }
     }
+    var isHeadLine: Bool {
+        switch self {
+        case .headLine(_):
+            return true
+        default:
+            return false
+        }
+    }
 }
+
+func <= (left: LineType, right: LineType) -> Bool { return left.level <= right.level }
+func >= (left: LineType, right: LineType) -> Bool { return left.level >= right.level }
+
 class Node {
     let childs = [Node]()
-    let parent:Node?
+    let parent: Node?
     let type: LineType
     let line: String
-    
-    init(parent: Node?, type:LineType, line:String){
+
+    init(parent: Node?, type: LineType, line: String){
         self.parent = parent
         self.type = type
         self.line = line
         parent?.childs.append(self)
     }
 }
-func <= (left:LineType, right:LineType) -> Bool { return left.level() <= right.level() }
-func >= (left:LineType, right:LineType) -> Bool { return left.level() >= right.level() }
+
+func extractText(text: String, hierarchy: [String], options: [Option:[String]]) -> String {
+    let root = createTree(text: text, options: options)
+    guard let nodes = checkStructure(rootNode: root, hierarchy: hierarchy) else { return "" }
+    var str = ""
+    for node in nodes {
+        str += getStringWith(rootNode: node)
+    }
+    return str
+}
+func createTree(text: String, options: [Option:[String]]) -> Node {
+    var current = Node(parent: nil, type: .none, line: "")
+    let root = current
+
+    let disabledTabOption = (options[ConsoleOption.tab.option] == nil)
+    let disabledEmptyLineOption = (options[ConsoleOption.emptyline.option] == nil)
+
+    text.enumerateLines(invoking: { line, stop in
+        let trimmedLine = disabledTabOption ? line.trimmingCharacters(in: .whitespaces) : line
+        if disabledEmptyLineOption && trimmedLine.isEmpty { return }
+
+        let type: LineType = trimmedLine.headLineLevel == 0 ? .content : .headLine(trimmedLine.headLineLevel)
+
+        while current.type >= type { current = current.parent! }
+        current = Node(parent: current, type: type, line: trimmedLine)
+    })
+    return root
+}
+func checkStructure(rootNode: Node, hierarchy: [String]) -> [Node]? {
+    var hierarchy = hierarchy
+    var nodes = [rootNode]
+
+    while hierarchy.count > 0 {
+        for node in nodes {
+            nodes = node.childs.filter { $0.type.isHeadLine && $0.line.planeText == hierarchy.first }
+            if nodes.count == 0 { return nil }
+            hierarchy.remove(at: 0)
+        }
+    }
+    return nodes
+}
+func getStringWith(rootNode: Node) -> String {
+    var str = ""
+    for child in rootNode.childs {
+        str += child.line + "\n"
+        str += getStringWith(rootNode: child)
+    }
+    return str
+}
 
 // md-tree -f filename.md 大見出し 中見出し 小見出し
 private func main(arguments: [String]) {
     let arguments = arguments.dropFirst()
 
-    let fileOption = Option(trigger: OptionTrigger.Mixed("f", "filename"), numberOfParameters: 1, helpDescription: "Markdown file")
-    let parser = OptionParser(definitions: [fileOption])
+    let parser = OptionParser(definitions: [ConsoleOption.file.option, ConsoleOption.tab.option, ConsoleOption.emptyline.option])
 
     do {
         let (options, _) = try parser.parse(parameters: Array(arguments))
-        if let filePath = options[fileOption]?.first {
-            print(filePath)
-            let location = NSString(string: filePath).expandingTildeInPath
-            let fileContent = try String(contentsOfFile: location, encoding: String.Encoding.utf8)
-            let hierarchy = Array(arguments.dropFirst(2))
+        if let filePath = options[ConsoleOption.file.option]?.first {
+            let absolutePath = NSString(string: filePath).expandingTildeInPath
+            let fileContent = try String(contentsOfFile: absolutePath, encoding: String.Encoding.utf8)
 
-            _ = extract(text: fileContent,hierarchy: hierarchy)
+            let filteredArgs = arguments.filter { $0 != "-t" && $0 != "-tab" }
+                                        .filter { $0 != "-e" && $0 != "-emptyline" }
+
+            let hierarchy = Array(filteredArgs.dropFirst(2))
+            if hierarchy.count == 0 { exit(0) }
+
+            print(extractText(text: fileContent, hierarchy: hierarchy, options: options))
         }
     } catch let OptionKitError.InvalidOption(description: description) {
         print(description)
         exit(EXIT_FAILURE)
     } catch {
         exit(0)
-    }
-}
-
-func extract(text: String, hierarchy: [String]) -> String{
-    var current = Node(parent: nil, type: .none, line: "")
-    let root = current
-    text.enumerateLines(invoking: { line, stop in
-        let trimmedLine = line.trimmingCharacters(in: .whitespaces)
-        if trimmedLine.isEmpty { return }
-        let type:LineType = trimmedLine.headLineLevel() == 0 ? .content : .headLine(trimmedLine.headLineLevel())
-        
-        while(current.type >= type){ current = current.parent! }
-        current = Node(parent: current, type: type, line: trimmedLine)
-    })
-    
-    show(node: root)
-    return ""
-}
-func show(node:Node){
-    for child in node.childs {
-        print(child.line)
-        show(node: child)
     }
 }
 
